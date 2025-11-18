af3f018533b8003c9bb83002ef16a9cff05b8b85
Avishka Sewwandi <wmavishkasewwandi@gmail.com>
2025-11-18 11:17:46 +0530

    .

--- Base/api.js
+++ Base/api.js
@@ -1,5 +1,5 @@
 // const BASE_URL = 'https://apexqa.alevelclothing.lk/api';
 //const BASE_URL = "https://apex.alevelclothing.lk/api";
-const BASE_URL = "https://posuat.openskylabz.com/api";
+const BASE_URL = "https://opusmarketplace.clovesis.com/api";
 
 export default BASE_URL;
